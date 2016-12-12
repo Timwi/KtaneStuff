2fb39a904f5d9dd97685f2d8c7318ea0416faea4
Timwi <github@timwi.de>
2016-12-12 20:19:26 +0100

    Barebones

---
+++ Src/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("KtaneStuff")]
+[assembly: AssemblyDescription("Contains some ancillary code used in the creation of some Keep Talking and Nobody Explodes mods.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("KtaneStuff")]
+[assembly: AssemblyCopyright("Copyright © Timwi 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("95055383-2e25-42be-97b7-e1411a695e1d")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+namespace KtaneStuff
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Foo");
+        }
+    }
+}
