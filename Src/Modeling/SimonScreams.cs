b4f9e4625cecbec78e3560fb6e8c5e68b964f66d
Timwi <github@timwi.de>
2016-12-19 12:11:38 +0100

    Simon Screams

--- Src/Modeling/SimonScreams.cs
+++ Src/Modeling/SimonScreams.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
 using System.IO;
 using RT.Util;
 
 namespace KtaneStuff.Modeling
 {
+    using RT.Util.Geometry;
     using static Md;
 
     static class SimonScreams
@@ -12,14 +14,14 @@ namespace KtaneStuff.Modeling
         public static void Do()
         {
             File.WriteAllText(@"D:\c\KTANE\SimonScreams\Assets\Models\Button.obj", GenerateObjFile(Button(), "Button"));
-            //File.WriteAllText(@"D:\c\KTANE\SimonScreams\Assets\Models\ButtonHighlight.obj", GenerateObjFile(ButtonHighlight(), "ButtonHighlight"));
+            File.WriteAllText(@"D:\c\KTANE\SimonScreams\Assets\Models\ButtonHighlight.obj", GenerateObjFile(ButtonHighlight(), "ButtonHighlight"));
         }
 
         private static IEnumerable<VertexInfo[]> Button()
         {
-            var height = .04;
+            var height = .2;
             var fh = height * .4;
-            var innerRadius = 0.3;
+            var innerRadius = 0.4;
             var outerRadius = 1.0;
             var fr = innerRadius * .1;
             var angle = 30.0;
@@ -37,14 +39,26 @@ namespace KtaneStuff.Modeling
                 pt((innerRadius - fr) * cos(angle), fh, (innerRadius - fr) * sin(angle)), pt(innerRadius * cos(angle / 2), height, innerRadius * sin(angle / 2)), pt((innerRadius + outerRadius) / 2, height, 0), pt(frox, fh, -froy),
                 pt(innerRadius * cos(angle), 0, innerRadius * sin(angle)), pt(frix, fh, friy), pt(frox, fh, froy), pt(outerRadius, 0, 0),
                 bézierSteps);
-            return CreateMesh(false, false, Ut.NewArray(bézierSteps, bézierSteps, (x, y) => new MeshVertexInfo(patchPiece[x][y], x == bézierSteps - 1 ? Normal.Mine : Normal.Average, x == 0 ? Normal.Mine : Normal.Average, y == bézierSteps - 1 ? Normal.Mine : Normal.Average, y == 0 ? Normal.Mine : Normal.Average)));
+            return CreateMesh(false, false, Ut.NewArray(bézierSteps, bézierSteps, (x, y) => new MeshVertexInfo(patchPiece[x][y].Add(x: .03), x == bézierSteps - 1 ? Normal.Mine : Normal.Average, x == 0 ? Normal.Mine : Normal.Average, y == bézierSteps - 1 ? Normal.Mine : Normal.Average, y == 0 ? Normal.Mine : Normal.Average)));
         }
 
         private static IEnumerable<VertexInfo[]> ButtonHighlight()
         {
-            var width = .11;
-            var innerWidth = .09;
-            return CreateMesh(false, true, Ut.NewArray(2, 4, (i, j) => (j * 90 + 45).Apply(angle => (i == 0 ? innerWidth : width).Apply(radius => pt(radius * cos(angle), 0, radius * sin(angle))))));
+            var preRadius = .03;
+            var innerRadius = .44;
+            var outerRadius = 1.15;
+            var angle = 32.0;
+
+            var holeInnerRadius = .4;
+            var holeOuterRadius = 1.0;
+            var holeAngle = 30.0;
+
+            return
+                Triangulate(Ut.NewArray(
+                    new[] { p(-preRadius, 0), p(innerRadius * cos(-angle), innerRadius * sin(-angle)), p(outerRadius, 0), p(innerRadius * cos(angle), innerRadius * sin(angle)) },
+                    new[] { p(0, 0), p(holeInnerRadius * cos(holeAngle), holeInnerRadius * sin(holeAngle)), p(holeOuterRadius, 0), p(holeInnerRadius * cos(-holeAngle), holeInnerRadius * sin(-holeAngle)) }.Select(p => p + new PointD(.03, 0))
+                ))
+                    .Select(poly => poly.Select(p => pt(p.X, 0, p.Y).WithNormal(0, 1, 0)).ToArray());
         }
     }
 }
