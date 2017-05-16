aedab76f897c8be6171df70203f3973ffbe27233
Timwi <github@timwi.de>
2017-05-16 16:32:49 +0200

    LED Encryption cheat sheet.

---
+++ Src/LedEncryption.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using RT.TagSoup;
+
+namespace KtaneStuff
+{
+    static class LedEncryption
+    {
+        public static void DoCheatSheet()
+        {
+            var values = new[] { 2, 3, 4, 5, 6, 7 };
+            var names = new[] { "Red", "Green", "Blue", "Yellow", "Purple", "Orange" };
+
+            Utils.ReplaceInFile(@"D:\c\KTANE\HTML\LED Encryption cheat sheet (Timwi).html", "<!--%%-->", "<!--%%%-->",
+                new TABLE(
+                    new TR(names.Select(n => new TH(n))),
+                    Enumerable.Range(0, 26).Select(c => new TR(Enumerable.Range(0, values.Length).Select(i =>
+                         new TD($"{(char) ('A' + c)}{(char) ('A' + (c * values[i]) % 26)}")
+                    )))
+                ).ToString());
+        }
+    }
+}
