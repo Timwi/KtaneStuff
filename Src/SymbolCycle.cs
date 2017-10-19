ff639f74c9091fcb2f2bafacd808a7e2d5fa43ff
Timwi <github@timwi.de>
2017-10-19 22:28:03 +0200

    Stuff for Symbol Cycle

--- Src/SymbolCycle.cs
+++ Src/SymbolCycle.cs
@@ -20,6 +20,100 @@ namespace KtaneStuff
         {
             File.WriteAllText(@"D:\c\KTANE\SymbolCycle\Assets\Models\Plate.obj", GenerateObjFile(fixTextures(Plate()), "Plate"));
             File.WriteAllText(@"D:\c\KTANE\SymbolCycle\Assets\Models\ScreenHighlight.obj", GenerateObjFile(ScreenHighlight(), "ScreenHighlight"));
+            File.WriteAllText(@"D:\c\KTANE\SymbolCycle\Assets\Models\Switch.obj", GenerateObjFile(Switch(), "Switch"));
+            File.WriteAllText(@"D:\c\KTANE\SymbolCycle\Assets\Models\SwitchCasing.obj", GenerateObjFile(SwitchCasing(), "SwitchCasing"));
+        }
+
+        private static IEnumerable<VertexInfo[]> Switch()
+        {
+            const int revSteps = 9;
+            const double switchAngle = 30;
+            const double extraAngle = 10;
+            const double depth = .5;
+            const double r = 1.5;
+            const double bw = .1;
+
+            // Outer frame (rounded part and front and back)
+            foreach (var baseAngle in new[] { 0, 180 - extraAngle })
+            {
+                foreach (var face in CreateMesh(false, false, Enumerable.Range(0, revSteps).Select(i => (switchAngle + extraAngle) * i / (revSteps - 1) + baseAngle).Select(angle => Ut.NewArray(
+                    pt(0, 0, -depth).WithMeshInfo(0, 0, -1),
+                    pt(r * cos(angle), r * sin(angle), -depth).WithMeshInfo(0, 0, -1),
+                    pt((r + bw) * cos(angle), (r + bw) * sin(angle), -depth + bw).WithMeshInfo(cos(angle), sin(angle), 0),
+                    pt((r + bw) * cos(angle), (r + bw) * sin(angle), depth - bw).WithMeshInfo(cos(angle), sin(angle), 0),
+                    pt(r * cos(angle), r * sin(angle), depth).WithMeshInfo(0, 0, 1),
+                    pt(0, 0, depth).WithMeshInfo(0, 0, 1)
+                )).ToArray()))
+                    yield return face;
+            }
+
+            // Rest of the front plate
+            yield return new[] { pt(0, 0, -depth).WithNormal(0, 0, -1), pt(r * cos(switchAngle + extraAngle), r * sin(switchAngle + extraAngle), -depth).WithNormal(0, 0, -1), pt(r * cos(180 - extraAngle), r * sin(180 - extraAngle), -depth).WithNormal(0, 0, -1) }.Reverse().ToArray();
+            // Rest of the back plate
+            yield return new[] { pt(0, 0, depth).WithNormal(0, 0, 1), pt(r * cos(switchAngle + extraAngle), r * sin(switchAngle + extraAngle), depth).WithNormal(0, 0, 1), pt(r * cos(180 - extraAngle), r * sin(180 - extraAngle), depth).WithNormal(0, 0, 1) };
+
+            var bAngle = 270 - switchAngle / 2;
+            var data = Ut.NewArray(
+                Ut.NewArray(
+                    pt(r + bw, 0, -depth + bw).WithMeshInfo(1, 0, 0),
+                    pt(r, 0, -depth).WithMeshInfo(0, 0, -1),
+                    pt(0, 0, -depth).WithMeshInfo(0, 0, -1),
+                    pt(r * cos(switchAngle + 180), r * sin(switchAngle + 180), -depth).WithMeshInfo(0, 0, -1),
+                    pt((r + bw) * cos(switchAngle + 180), (r + bw) * sin(switchAngle + 180), -depth + bw).WithMeshInfo(-cos(switchAngle), -sin(switchAngle), 0)
+                ),
+                Ut.NewArray(
+                    pt(r + bw, 0, -depth + bw).WithMeshInfo(1, 0, 0),
+                    pt(r, -bw, -depth + bw).WithMeshInfo(0, -1, 0),
+                    pt(bw * cos(bAngle), bw * sin(bAngle), -depth + bw).WithMeshInfo(cos(bAngle), sin(bAngle), 0),
+                    pt(r * cos(switchAngle + 180) + bw * cos(switchAngle - 90), r * sin(switchAngle + 180) + bw * sin(switchAngle - 90), -depth + bw).WithMeshInfo(cos(switchAngle - 90), sin(switchAngle - 90), 0),
+                    pt((r + bw) * cos(switchAngle + 180), (r + bw) * sin(switchAngle + 180), -depth + bw).WithMeshInfo(cos(switchAngle + 180), sin(switchAngle + 180), 0)
+                ),
+                Ut.NewArray(
+                    pt(r + bw, 0, depth - bw).WithMeshInfo(1, 0, 0),
+                    pt(r, -bw, depth - bw).WithMeshInfo(0, -1, 0),
+                    pt(bw * cos(bAngle), bw * sin(bAngle), depth - bw).WithMeshInfo(cos(bAngle), sin(bAngle), 0),
+                    pt(r * cos(switchAngle + 180) + bw * cos(switchAngle - 90), r * sin(switchAngle + 180) + bw * sin(switchAngle - 90), depth - bw).WithMeshInfo(cos(switchAngle - 90), sin(switchAngle - 90), 0),
+                    pt((r + bw) * cos(switchAngle + 180), (r + bw) * sin(switchAngle + 180), depth - bw).WithMeshInfo(cos(switchAngle + 180), sin(switchAngle + 180), 0)
+                ),
+                Ut.NewArray(
+                    pt(r + bw, 0, depth - bw).WithMeshInfo(1, 0, 0),
+                    pt(r, 0, depth).WithMeshInfo(0, 0, 1),
+                    pt(0, 0, depth).WithMeshInfo(0, 0, 1),
+                    pt(r * cos(switchAngle + 180), r * sin(switchAngle + 180), depth).WithMeshInfo(0, 0, 1),
+                    pt((r + bw) * cos(switchAngle + 180), (r + bw) * sin(switchAngle + 180), depth - bw).WithMeshInfo(cos(switchAngle + 180), sin(switchAngle + 180), 0)
+                )
+            );
+
+            foreach (var face in CreateMesh(false, false, data))
+                yield return face;
+        }
+
+        private static IEnumerable<VertexInfo[]> SwitchCasing()
+        {
+            const int revSteps = 17;
+            const double w = .95;     // width of the whole casing
+            const double u = .8;   // width of the hole for the switch
+            const double h = .45;    // height of the whole casing
+            const double v = .25;    // height of the hole for the switch
+            const double r = .25;
+            const double bw = .05;
+            const double depth = .2;
+            const double ic = .05;
+            const double c = .025;
+
+            return CreateMesh(true, false, Ut.NewArray(
+                Ut.NewArray(pt(u - ic, v, bw).WithNormal(0, -1, 0), pt(u - ic, v + bw, 0).WithNormal(0, 0, -1), pt(w - c, h, 0).WithNormal(0, 0, -1), pt(w - c, h + bw, bw).WithNormal(0, 1, 0), pt(w - c, h + bw, depth).WithNormal(0, 1, 0)),
+                Ut.NewArray(pt(u, v - ic, bw).WithNormal(-1, 0, 0), pt(u + bw, v - ic, 0).WithNormal(0, 0, -1), pt(w, h - c, 0).WithNormal(0, 0, -1), pt(w + bw, h - c, bw).WithNormal(1, 0, 0), pt(w + bw, h - c, depth).WithNormal(1, 0, 0))
+            ).Concat(Enumerable.Range(0, revSteps).Select(i => 180.0 * i / (revSteps - 1)).Select(angle => Ut.NewArray(
+                pt(u, v / 2 - v * angle / 180, bw).WithNormal(-1, 0, 0),
+                pt(u + bw, v / 2 - v * angle / 180, 0).WithNormal(0, 0, -1),
+                pt(w + r * sin(angle), r * cos(angle), 0).WithNormal(0, 0, -1),
+                pt(w + (r + bw) * sin(angle) + bw * (cos(2 * angle) + 1) / 2, (r + bw) * cos(angle), bw).WithNormal(sin(angle), cos(angle), 0),
+                pt(w + (r + bw) * sin(angle) + bw * (cos(2 * angle) + 1) / 2, (r + bw) * cos(angle), depth).WithNormal(sin(angle), cos(angle), 0)))
+            ).Concat(Ut.NewArray(
+                Ut.NewArray(pt(u, -v + ic, bw).WithNormal(-1, 0, 0), pt(u + bw, -v + ic, 0).WithNormal(0, 0, -1), pt(w, -h + c, 0).WithNormal(0, 0, -1), pt(w + bw, -h + c, bw).WithNormal(1, 0, 0), pt(w + bw, -h + c, depth).WithNormal(1, 0, 0)),
+                Ut.NewArray(pt(u - ic, -v, bw).WithNormal(0, 1, 0), pt(u - ic, -v - bw, 0).WithNormal(0, 0, -1), pt(w - c, -h, 0).WithNormal(0, 0, -1), pt(w - c, -h - bw, bw).WithNormal(0, -1, 0), pt(w - c, -h - bw, depth).WithNormal(0, -1, 0))
+            )).Apply(arr => arr.Select(face => face.Select(vx => vx.Location.WithMeshInfo(vx.Normal.Value)).ToArray()).Concat(arr.Select(face => face.Select(vx => vx.RotateZ(180)).Select(vx => vx.Location.WithMeshInfo(vx.Normal.Value)).ToArray()))).Reverse().ToArray());
         }
 
         private static IEnumerable<VertexInfo[]> ScreenHighlight()
@@ -92,9 +186,9 @@ namespace KtaneStuff
                 }
 
             const double displacementX = .67;
-            const double displacementY = .05;
+            const double displacementY = .1;
             const double wx = 0;
-            const double wy = .6;
+            const double wy = .65;
 
             foreach (var face in CreateMesh(true, false, infs.Select(inf => Ut.NewArray(
                 rPt(innerRadius, inf.Angle + 90 * inf.Quadrant, inf.Quadrant, innerDepth, displacementX, displacementY).Add(x: wx, z: wy).WithMeshInfo(Normal.Average, Normal.Average, Normal.Mine, Normal.Mine),
@@ -106,11 +200,6 @@ namespace KtaneStuff
 
             foreach (var face in platePoly.Triangulate())
                 yield return face.Reverse().Select(p => pt(p.X, .15, p.Y).WithNormal(0, 1, 0)).ToArray();
-        }
-
-        private static IEnumerable<VertexInfo[]> fixTextures(IEnumerable<VertexInfo[]> original)
-        {
-            return original.Select(o => o.Select(v => v.WithTexture(new PointD(.4771284794 * v.Location.X + .46155, -.4771284794 * v.Location.Z + .5337373145))).ToArray());
         }
 
         public static void CreateIcons()
@@ -149,6 +238,11 @@ namespace KtaneStuff
                 }).Save($@"D:\c\KTANE\SymbolCycle\Assets\Symbols\Icon{j}.png");
                 j++;
             }
+        }
+
+        private static IEnumerable<VertexInfo[]> fixTextures(IEnumerable<VertexInfo[]> original)
+        {
+            return original.Select(o => o.Select(v => v.WithTexture(new PointD(.4771284794 * v.Location.X + .46155, -.4771284794 * v.Location.Z + .5337373145))).ToArray());
         }
     }
 }
