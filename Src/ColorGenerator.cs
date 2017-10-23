b3b6e37cc4fe689c8d5ffedcb7069efe1d8ad0e9
Timwi <github@timwi.de>
2017-10-23 22:21:58 +0200

    Color Generator cheat sheet: both buggy and fixed versions.

--- Src/ColorGenerator.cs
+++ Src/ColorGenerator.cs
@@ -9,13 +9,25 @@ namespace KtaneStuff
     {
         public static void GenerateCheatSheet()
         {
-            string ch(int n) => Enumerable.Range(0, 10).Where(v => v == n).Select(v => (char) ('0' + v)).Concat(Enumerable.Range(0, 26).Where(v => (v + 1) % 16 == n).Select(v => (char) ('A' + v))).JoinString();
-            Utils.ReplaceInFile(@"D:\c\KTANE\Public\HTML\Color Generator cheat sheet (Timwi).html", "<!--%%-->", "<!--%%%-->",
-                new TABLE { class_ = "color-generator" }._(
-                    new TR(new TD { class_ = "corner" }, Enumerable.Range(0, 16).Select(i => new TH(ch(i)))),
-                    Enumerable.Range(0, 16).Select(row => new TR(
-                        new TH(ch(row)),
-                        Enumerable.Range(0, 16).Select(col => new TD(col * 16 + row))))).ToString());
+            {
+                string ch(int n) => Enumerable.Range(0, 10).Where(v => v == n).Select(v => (char) ('0' + v)).Concat(Enumerable.Range(0, 26).Where(v => (v + 1) % 16 == n).Select(v => (char) ('A' + v))).JoinString();
+                Utils.ReplaceInFile(@"D:\c\KTANE\Public\HTML\Color Generator cheat sheet (Timwi).html", "<!--%%-->", "<!--%%%-->",
+                    new TABLE { class_ = "color-generator" }._(
+                        new TR(new TD { class_ = "corner" }, Enumerable.Range(0, 16).Select(i => new TH(ch(i)))),
+                        Enumerable.Range(0, 16).Select(row => new TR(
+                            new TH(ch(row)),
+                            Enumerable.Range(0, 16).Select(col => new TD(col * 16 + row))))).ToString());
+            }
+
+            {
+                string ch(int n) => Enumerable.Range(0, 10).Where(v => v == n).Select(v => (char) ('0' + v)).Concat(Enumerable.Range(0, 26).Where(v => v % 16 == n).Select(v => (char) ('A' + v))).JoinString();
+                Utils.ReplaceInFile(@"D:\c\KTANE\Public\HTML\Color Generator cheat sheet (Timwi).html", "<!--%%2-->", "<!--%%%2-->",
+                    new TABLE { class_ = "color-generator" }._(
+                        new TR(new TD { class_ = "corner" }, Enumerable.Range(0, 16).Select(i => new TH(ch(i)))),
+                        Enumerable.Range(0, 16).Select(row => new TR(
+                            new TH(ch(row)),
+                            Enumerable.Range(0, 16).Select(col => new TD(col * 16 + row))))).ToString());
+            }
         }
     }
 }
