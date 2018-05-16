394af4d13293888aa740e3acbd47a59fe90799fa
Timwi <github@timwi.de>
2018-05-16 18:27:14 +0200

    Marble Tumble (includes automatic solver)

--- Src/MarbleTumble.cs
+++ Src/MarbleTumble.cs
@@ -4,6 +4,7 @@ using System.Drawing;
 using System.IO;
 using System.Linq;
 using KtaneStuff.Modeling;
+using RT.Dijkstra;
 using RT.Util;
 using RT.Util.Drawing;
 using RT.Util.ExtensionMethods;
@@ -15,16 +16,16 @@ namespace KtaneStuff
 
     static class MarbleTumble
     {
-        const double height = 1;
-        const double bevelSize = .11;
-        const double marble = .38;
-        const double bevelRatio = .7;
-        const double bevMid = bevelSize * bevelRatio;
-        const double bevMid2 = bevelSize * (1 - bevelRatio);
+        const double _height = 1;
+        const double _bevelSize = .11;
+        const double _marbleSize = .38;
+        const double _bevelRatio = .7;
+        const double _bevMid = _bevelSize * _bevelRatio;
+        const double _bevMid2 = _bevelSize * (1 - _bevelRatio);
 
-        const int numNotches = 10;
-        static int[] notchMins = new[] { 3, 2, 1, 1, 1 };
-        static int[] notchMaxs = new[] { 7, 8, 9, 9, 9 };
+        const int _numNotches = 10;
+        static int[] _notchMins = new[] { 3, 2, 1, 1, 1 };
+        static int[] _notchMaxs = new[] { 7, 8, 9, 9, 9 };
 
         public static void DoModels()
         {
@@ -32,8 +33,8 @@ namespace KtaneStuff
                 if (file.Name.StartsWith("Cylinder-") && file.Name.EndsWith(".obj"))
                     file.Delete();
             for (int r = 1; r <= 5; r++)
-                for (int t = notchMins[r - 1]; t <= notchMaxs[r - 1]; t++)
-                    File.WriteAllText($@"D:\c\KTANE\MarbleTumble\Assets\Models\Cylinder-{r}-{t}.obj", GenerateObjFile(Cylinder(r - .5, r + .5, (360 / numNotches) * t), $"Cylinder_{r}_{t}"));
+                for (int t = _notchMins[r - 1]; t <= _notchMaxs[r - 1]; t++)
+                    File.WriteAllText($@"D:\c\KTANE\MarbleTumble\Assets\Models\Cylinder-{r}-{t}.obj", GenerateObjFile(Cylinder(r - .5, r + .5, (360 / _numNotches) * t), $"Cylinder_{r}_{t}"));
         }
 
         private static IEnumerable<double> range(double from, double to, int steps) => Enumerable.Range(0, steps).Select(i => from + (to - from) * i / steps);
@@ -80,17 +81,17 @@ namespace KtaneStuff
 
         private static IEnumerable<VertexInfo[]> Cylinder(double inner, double outer, int trap)
         {
-            var topFaceOutline = CylinderPolygon(inner + bevelSize, outer - bevelSize, marble + bevelSize, marble - bevelSize, trap, 0);
+            var topFaceOutline = CylinderPolygon(inner + _bevelSize, outer - _bevelSize, _marbleSize + _bevelSize, _marbleSize - _bevelSize, trap, 0);
 
-            var bottom = CylinderPolygon(inner, outer, marble, marble, trap, 1).Select(p => pt(p.p.X, 0, p.p.Y).WithMeshInfo(Normal.Mine, Normal.Mine, Normal.Average, Normal.Average).WithTexture(p.texture)).ToArray();
-            var bevelBottom = CylinderPolygon(inner, outer, marble, marble, trap, .1).Select(p => pt(p.p.X, height, p.p.Y).WithMeshInfo(Normal.Mine, Normal.Theirs, Normal.Average, Normal.Average).WithTexture(p.texture)).ToArray();
-            var bevelMiddle = CylinderPolygon(inner + bevMid2, outer - bevMid2, marble + bevMid2, marble - bevMid2, trap, .05).Select(p => pt(p.p.X, height + bevMid, p.p.Y).WithMeshInfo(Normal.Average, Normal.Average, Normal.Average, Normal.Average).WithTexture(p.texture)).ToArray();
-            var bevelTop = topFaceOutline.Select(p => pt(p.p.X, height + bevelSize, p.p.Y).WithMeshInfo(0, 1, 0).WithTexture(p.texture)).ToArray();
+            var bottom = CylinderPolygon(inner, outer, _marbleSize, _marbleSize, trap, 1).Select(p => pt(p.p.X, 0, p.p.Y).WithMeshInfo(Normal.Mine, Normal.Mine, Normal.Average, Normal.Average).WithTexture(p.texture)).ToArray();
+            var bevelBottom = CylinderPolygon(inner, outer, _marbleSize, _marbleSize, trap, .1).Select(p => pt(p.p.X, _height, p.p.Y).WithMeshInfo(Normal.Mine, Normal.Theirs, Normal.Average, Normal.Average).WithTexture(p.texture)).ToArray();
+            var bevelMiddle = CylinderPolygon(inner + _bevMid2, outer - _bevMid2, _marbleSize + _bevMid2, _marbleSize - _bevMid2, trap, .05).Select(p => pt(p.p.X, _height + _bevMid, p.p.Y).WithMeshInfo(Normal.Average, Normal.Average, Normal.Average, Normal.Average).WithTexture(p.texture)).ToArray();
+            var bevelTop = topFaceOutline.Select(p => pt(p.p.X, _height + _bevelSize, p.p.Y).WithMeshInfo(0, 1, 0).WithTexture(p.texture)).ToArray();
 
             var mesh = CreateMesh(false, true, new[] { bottom, bevelBottom, bevelMiddle, bevelTop });
             try
             {
-                var topFace = Md.Triangulate(topFaceOutline.Reverse().Select(p => p.p)).Select(face => face.Select(p => pt(p.X, height + bevelSize, p.Y).WithNormal(0, 1, 0).WithTexture(topFaceOutline.MinElement(p2 => p2.p.Distance(p)).texture)).ToArray()).ToArray();
+                var topFace = Md.Triangulate(topFaceOutline.Reverse().Select(p => p.p)).Select(face => face.Select(p => pt(p.X, _height + _bevelSize, p.Y).WithNormal(0, 1, 0).WithTexture(topFaceOutline.MinElement(p2 => p2.p.Distance(p)).texture)).ToArray()).ToArray();
                 var allFaces = mesh.Concat(topFace);
 
                 var allTextures = allFaces.SelectMany(f => f).Select(v => v.Texture.Value);
@@ -110,7 +111,7 @@ namespace KtaneStuff
 
         public static void DoDebug()
         {
-            makeTempPng(CylinderPolygon(.5, 1.5, marble, marble, 120, 0).Select(p => p.p));
+            makeTempPng(CylinderPolygon(.5, 1.5, _marbleSize, _marbleSize, 120, 0).Select(p => p.p));
         }
 
         private static void makeTempPng(IEnumerable<PointD> polygon)
@@ -130,53 +131,183 @@ namespace KtaneStuff
                     }
                     foreach (var point in polygon)
                         drawPoint(point, Brushes.Black);
-
-                    //double inner = .5;
-                    //double outer = 1.5;
-                    //double trapFillet = marble;
-                    //int trap = 120;
-                    //double textureDist = 0;
-                    //double w = (outer - inner) / 2;     // half the width of the cylinder’s bulk (same as radius of the semicircles on each side of the gap)
-                    //double m = (outer + inner) / 2;     // distance from center to the middle of the bulk
-                    //double gapAngle = arcsin(marble / m) + arcsin(w / m);               // angle between the middle of the gap and the centerpoint of the semicircles on either side of it
-                    //double trapAngle = arcsin(marble / (outer - trapFillet)) + arcsin(trapFillet / (outer - trapFillet));   // angle between the middle of the trap and the centerpoint of the fillet
-                    //double t1 = trap - trapAngle;       // angle of the centerpoint of the first trap fillet
-                    //double t2 = trap + trapAngle;       // angle of the centerpoint of the other trap fillet
-
-                    //(PointD p, PointD texture) mk(double rMin, double θMin, double rMaj, double θMaj) => (
-                    //    p(rMin * cos(θMin) + rMaj * cos(θMaj), rMin * sin(θMin) + rMaj * sin(θMaj)),
-                    //    p((rMin + textureDist) * cos(θMin) + rMaj * cos(θMaj), (rMin + textureDist) * sin(θMin) + rMaj * sin(θMaj)));
-
-                    //var trapFillet1 = range(0, 90, 6 * 4).Select(i => mk(trapFillet, i + t1, outer - trapFillet, t1));
-                    //var trapBottom = range(270, 90, 12 * 4).Select(i => mk(marble, i + trap, outer - trapFillet, trap));
-                    //var trapFillet2 = range(-90, 0, 6 * 4).Select(i => mk(trapFillet, i + t2, outer - trapFillet, t2));
-
-                    //var mp = mk(0, 0, outer - trapFillet, t1);
-                    //drawPoint(mp.p, Brushes.Red);
-                    //g.DrawLine(new Pen(Brushes.Red, .05f), new PointF(0, 0), mp.p.ToPointF());
-                    //g.DrawLine(new Pen(Brushes.Red, .05f), mp.p.ToPointF(), mk(trapFillet, 0 + t1, outer - trapFillet, t1).p.ToPointF());
-                    //g.DrawLine(new Pen(Brushes.Red, .05f), mp.p.ToPointF(), mk(trapFillet, 90 + t1, outer - trapFillet, t1).p.ToPointF());
-
-                    //foreach (var tup in polygon.ConsecutivePairs(true))
-                    //    g.DrawLine(new Pen(Brushes.Black, .2f), tup.Item1.ToPointF(), tup.Item2.ToPointF());
-
-                    //foreach (var tri in triangulated)
-                    //    g.FillPolygon(Brushes.CornflowerBlue, tri.Select(v => v.ToPointF()).ToArray());
                 }
             }).Save(@"D:\temp\temp.png");
         }
 
-        public static void GenerateSvg()
+        public static void GenerateComponentSvg()
         {
             var path = @"D:\c\KTANE\Public\HTML\img\Component\Marble Tumble.svg";
             Utils.ReplaceInFile(path, @"<!--%%-->", @"<!--%%%-->", Enumerable.Range(0, 5).Select(ix => $@"<path d='M {
                 (Rnd.Next(0, 10) * Math.PI / 5).Apply(angle =>
-                    CylinderPolygon(ix + .5, ix + 1.5, marble, marble, (360 / numNotches) * Rnd.Next(notchMins[ix], notchMaxs[ix] + 1), 0)
+                    CylinderPolygon(ix + .5, ix + 1.5, _marbleSize, _marbleSize, (360 / _numNotches) * Rnd.Next(_notchMins[ix], _notchMaxs[ix] + 1), 0)
                         .Select(tup => tup.p.Rotated(angle))
                         .Select(p => p * 300 / 11 + new PointD(166, 348 - 166))
                         .Select(tup => $"{tup.X},{tup.Y}")
                         .JoinString(" "))
             } z' stroke-width='1' stroke='#000' fill='{Rnd.Next(64, 255).Apply(shade => $"#{shade.ToString("X2")}{shade.ToString("X2")}{shade.ToString("X2")}")}' />").JoinString());
+        }
+
+        public static void GenerateLogfileAnalyzerSvgs()
+        {
+            for (int cylIx = 0; cylIx < 5; cylIx++)
+                for (int trapIx = _notchMins[cylIx]; trapIx <= _notchMaxs[cylIx]; trapIx++)
+                    foreach (var color in "red,yellow,green,blue,silver".Split(',').Zip("ff8181,eff09a,81d682,8eb5ff,ccc".Split(','), (name, col) => (name, col)))
+                    {
+                        File.WriteAllText(
+                            $@"D:\c\KTANE\Public\More\img\Marble Tumble\Cylinder-{cylIx}-{trapIx}-{color.name}.svg",
+                            $@"<svg xmlns='http://www.w3.org/2000/svg' viewBox='-6 -6 12 12'><path d='M {CylinderPolygon(cylIx + .5, cylIx + 1.5, _marbleSize, _marbleSize, (360 / _numNotches) * trapIx, 0)
+                                .Select(tup => tup.p)
+                                .Select(tup => $"{tup.X},{tup.Y}")
+                                .JoinString(" ")} z' fill='#{color.col}' stroke-width='.06' stroke='#000' /></svg>");
+                    }
+        }
+
+        sealed class DijNode : Node<int, string>
+        {
+            public int[] Rotations { get; private set; }
+            public int[] Traps { get; private set; }
+            public int[] ColorIxs { get; private set; }
+            public int Marble { get; private set; }
+            public int LastSec { get; private set; }
+
+            public DijNode(int[] rotations, int[] traps, int[] colorIxs, int marble, int lastSec)
+            {
+                Rotations = rotations;
+                Traps = traps;
+                ColorIxs = colorIxs;
+                Marble = marble;
+                LastSec = lastSec;
+            }
+
+            private static readonly int[][] _rotationData = @"-1,1,-2,0,2;-2,1,2,-1,0;1,0,2,-2,-1;0,-1,-2,1,2;2,0,1,-1,-2;1,-2,-1,2,0;-2,2,0,1,-1;0,-1,1,2,-2;-1,2,0,-2,1;2,-2,-1,0,1".Split(';').Select(str => str.Split(',').Select(s => int.Parse(s)).ToArray()).ToArray();
+
+            public override bool IsFinal => Marble == 0;
+
+            public static int m(int v) => (v % 10 + 10) % 10;
+
+            public override IEnumerable<Edge<int, string>> Edges
+            {
+                get
+                {
+                    if (Marble == 0 || m(Traps[Marble - 1] + Rotations[Marble - 1]) == (Marble == 5 ? 0 : m(Rotations[Marble])))
+                        yield break;
+
+                    for (int sec = 0; sec < 10; sec++)
+                    {
+                        if (sec != LastSec)
+                        {
+                            for (int times = 1; times <= 2; times++)
+                            {
+                                var newRotations = Ut.NewArray(5, i => Rotations[i] + times * _rotationData[sec][ColorIxs[i]]);
+                                var newMarble = Marble;
+                                while (newMarble > 0 && (newMarble == 5 ? 0 : m(newRotations[newMarble])) == m(newRotations[newMarble - 1]))
+                                    newMarble--;
+                                var weight = 60 * ((LastSec == -1) ? 1 : (sec < LastSec) ? (LastSec - sec) : (LastSec + 10 - sec));
+                                if (Marble != newMarble && Marble - newMarble <= weight)
+                                    weight /= (Marble - newMarble);
+                                yield return new Edge<int, string>(weight, $@"{times}× at {sec}; rotations: {newRotations.JoinString(", ")}, marble: {newMarble}, weight: {weight}", new DijNode(newRotations, Traps, ColorIxs, newMarble, sec));
+                            }
+                        }
+                    }
+                }
+            }
+
+            public override bool Equals(Node<int, string> other)
+            {
+                if (!(other is DijNode oth))
+                    return false;
+                for (int i = 0; i < 5; i++)
+                    if (m(Rotations[i]) != m(oth.Rotations[i]))
+                        return false;
+                return Marble == oth.Marble && LastSec == oth.LastSec;
+            }
+
+            public override int GetHashCode()
+            {
+                const int b = 378551;
+                int a = 63689;
+                int hash = 12;
+
+                unchecked
+                {
+                    for (int i = 0; i < 5; i++)
+                    {
+                        hash = hash * a + m(Rotations[i]);
+                        a = a * b;
+                    }
+                    return hash + Marble + 17 * LastSec;
+                }
+            }
+
+            public override string ToString()
+            {
+                return $"Rotations: {Rotations.JoinString(", ")}; Marble: {Marble}";
+            }
+        }
+
+        public static void Solver()
+        {
+            var colors = getStrArr("Colors? ");
+            if (colors == null)
+                return;
+            var colorNames = "red,yellow,green,blue,silver".Split(',');
+            if (colors.Any(c => !colorNames.Any(cn => cn.EqualsNoCase(c))))
+                return;
+            var colorIxs = colors.Select(c => colorNames.IndexOf(c, StringComparer.InvariantCultureIgnoreCase)).Reverse().ToArray();
+            var gaps = getIntArr("Gaps? ");
+            if (gaps == null)
+                return;
+            var traps = getIntArr("Traps? ");
+            if (traps == null)
+                return;
+
+            try
+            {
+                var result = DijkstrasAlgorithm.Run(new DijNode(gaps.Reverse().ToArray(), gaps.Zip(traps, (g, t) => t - g).Reverse().ToArray(), colorIxs, 5, -1), 0, (a, b) => a + b, out var totalWeight);
+                Console.WriteLine(result.JoinString("\n"));
+                Console.WriteLine($"Total weight: {totalWeight}");
+            }
+            catch (DijkstraNoSolutionException<int, string> e)
+            {
+                Console.WriteLine("No solution found.");
+                var all = new HashSet<string>();
+                for (int a = 0; a < 10; a++)
+                    for (int b = 0; b < 10; b++)
+                        for (int c = 0; c < 10; c++)
+                            for (int d = 0; d < 10; d++)
+                                for (int f = 0; f < 10; f++)
+                                    all.Add($"{a},{b},{c},{d},{f}");
+                foreach (DijNode elem in e.HashSet)
+                {
+                    if (!all.Remove(elem.Rotations.Select(r => (r % 10 + 10) % 10).JoinString(",")))
+                        System.Diagnostics.Debugger.Break();
+                    if (elem.Rotations.All(r => (r % 10 + 10) % 10 == 0) || elem.Marble != 5)
+                        Console.WriteLine(elem);
+                }
+                Console.WriteLine($"Remaining:\n{all.JoinString("\n")}");
+                System.Diagnostics.Debugger.Break();
+            }
+        }
+
+        private static string[] getStrArr(string prompt)
+        {
+            Console.Write(prompt);
+            var input = Console.ReadLine();
+            if (input == null)
+                return null;
+            var spl = input.Split(',');
+            if (spl.Length != 5)
+                return null;
+            return spl;
+        }
+
+        private static int[] getIntArr(string prompt)
+        {
+            var spl = getStrArr(prompt);
+            if (spl == null || spl.Any(s => !int.TryParse(s, out _)))
+                return null;
+            return spl.Select(s => int.Parse(s)).ToArray();
         }
     }
 }
