0545d96ae67842bb41d8369a888f6d420fa37cec
Timwi <github@timwi.de>
2018-06-14 03:50:07 +0200

    Grid Matching analysis (dead-end)

--- Src/GridMatching.cs
+++ Src/GridMatching.cs
@@ -2,57 +2,60 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using RT.Util;
 using RT.Util.ExtensionMethods;
 
 namespace KtaneStuff
 {
     static class GridMatching
     {
-        private static int[] _solutionStates = new int[]
+        private static ulong[] _grids = new ulong[]
         {
-            4801614,
-            11514736,
-            15334525,
-            12676110,
-            15026679,
-            27395820,
-            8266786,
-            10065819,
-            16831939,
-            4766790,
-            4019783,
-            25639603,
-            32360091,
-            24475420,
-            13166525,
-            8785572
+            39378177601UL,
+            64499872570UL,
+            32484181835UL,
+            47046548784UL,
+            17108770296UL,
+            67161369174UL,
+            51985924635UL,
+            14469411799UL,
+            25731425460UL,
+            23420243268UL,
+            27236915887UL,
+            44207693997UL,
+            7191299719UL,
+            5336900789UL,
+            35778815767UL,
+            68311691418UL
         };
 
         public static void CreateCheatSheet()
         {
-            // Maps from column counts+row counts of WHITE pixels (e.g. "4234324334" = columns are 4,2,3,4,3; rows are 2,4,3,3,4) to index in _solutionStates
+            // Maps from column counts+row counts of WHITE pixels (e.g. "4234/4334" = columns are 4,2,3,4; rows are 4,3,3,4) to index in _solutionStates
             var dic = new Dictionary<string, int>();
 
-            for (int ix = 0; ix < _solutionStates.Length; ix++)
-            {
-                var sol = _solutionStates[ix];
-                var rowCounts = Enumerable.Range(0, 5).Select(row => Enumerable.Range(0, 5).Count(col => (sol & (1 << (5 * row + col))) != 0)).ToArray();
-                var colCounts = Enumerable.Range(0, 5).Select(col => Enumerable.Range(0, 5).Count(row => (sol & (1 << (5 * row + col))) != 0)).ToArray();
-
-                foreach (var key in new[] {
-                    colCounts.JoinString() + rowCounts.JoinString(),
-                    rowCounts.Reverse().JoinString() + colCounts.JoinString(),
-                    colCounts.Reverse().JoinString() + rowCounts.Reverse().JoinString(),
-                    rowCounts.JoinString() + colCounts.Reverse().JoinString()
-                })
-                {
-                    if (dic.ContainsKey(key))
+            for (int ix = 0; ix < _grids.Length; ix++)
+                for (int ox = 0; ox <= 2; ox++)
+                    for (int oy = 0; oy <= 2; oy++)
                     {
-                        Console.WriteLine($"Duplicate: {key}");
+                        var sol = _grids[ix];
+                        var rowCounts = Enumerable.Range(oy, 4).Select(row => Enumerable.Range(ox, 4).Count(col => ((sol >> (6 * row + col)) & 1) != 0)).ToArray();
+                        var colCounts = Enumerable.Range(ox, 4).Select(col => Enumerable.Range(oy, 4).Count(row => ((sol >> (6 * row + col)) & 1) != 0)).ToArray();
+
+                        foreach (var key in Ut.NewArray(
+                            colCounts.JoinString() + "/" + rowCounts.JoinString(),
+                            rowCounts.Reverse().JoinString() + "/" + colCounts.JoinString(),
+                            colCounts.Reverse().JoinString() + "/" + rowCounts.Reverse().JoinString(),
+                            rowCounts.JoinString() + "/" + colCounts.Reverse().JoinString()
+                        ))
+                        {
+                            if (dic.ContainsKey(key))
+                            {
+                                Console.WriteLine($"Duplicate: {key}");
+                            }
+                            dic[key] = ix;
+                        }
                     }
-                    dic[key] = ix;
-                }
-            }
         }
     }
 }
