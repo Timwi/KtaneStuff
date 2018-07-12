c4bec15b90eeb0136ca6aaef879784af7cc6c661
Timwi <github@timwi.de>
2018-07-12 05:51:44 +0200

    Code for Maritime Flags and Pattern Cube

--- Src/MaritimeFlags.cs
+++ Src/MaritimeFlags.cs
@@ -30,12 +30,88 @@ namespace KtaneStuff
 
         public static void DoModels()
         {
-            File.WriteAllText(@"D:\c\KTANE\MaritimeFlags\Assets\Models\Compass.obj", GenerateObjFile(Compass(), "Compass"));
+            var (frame, dial) = Compass();
+            File.WriteAllText(@"D:\c\KTANE\MaritimeFlags\Assets\Models\Compass.obj", GenerateObjFile(frame, "Compass"));
+            File.WriteAllText(@"D:\c\KTANE\MaritimeFlags\Assets\Models\CompassBase.obj", GenerateObjFile(dial, "CompassBase"));
+            File.WriteAllText(@"D:\c\KTANE\MaritimeFlags\Assets\Models\CompassNeedle.obj", GenerateObjFile(CompassNeedle(), "CompassNeedle"));
         }
 
-        private static IEnumerable<Pt[]> Compass()
+        private static IEnumerable<VertexInfo[]> CompassNeedle()
         {
-            throw new NotImplementedException();
+            var w = .3;
+            var h = .7;
+            yield return new[] { pt(-w, 0, 0), pt(w, 0, 0), pt(0, 0, -h) }.FlatNormals();
+        }
+
+        private static (IEnumerable<VertexInfo[]> frame, IEnumerable<VertexInfo[]> dial) Compass()
+        {
+            const double rO = 1; // outer radius
+            const double rI = .9; // inner radius
+            const double rT = .75; // tick radius
+            const double hT = .1;   // height of the top of a tick
+            const double hP = .2;   // height of the top of the protrusions
+            const double hW = .15;   // height of the between-walls
+            const double aT = 6; // tick angle
+            const double v = 360.0 / 16;
+
+            var rnd = new Random(47);
+
+            var data = Enumerable.Range(0, 16).Select(i =>
+            {
+                Pt r(double angle, double rds, double y) => pt(rds * cos(angle), y, rds * sin(angle));
+
+                VertexInfo[] addTexture(VertexInfo[] face)
+                {
+                    const double txRadius = .05;
+                    //var mid = p(rnd.NextDouble() * (1 - 2 * txRadius) + txRadius, rnd.NextDouble() * (1 - 2 * txRadius) + txRadius);
+                    var mid = p(face[0].Location.X.Clip(txRadius, 1 - txRadius), face[0].Location.Z.Clip(txRadius, 1 - txRadius));
+                    var angle = rnd.NextDouble() * 360;
+                    return face.Select((vt, ix) => vt.WithTexture(mid + txRadius * p(cos(angle + ix * 360 / face.Length), sin(angle + ix * 360 / face.Length)))).ToArray();
+                }
+
+                var aL = v * i - aT / 2;        // angle of the left side of the tick and protrusion
+                var aMid = v * i;               // angle of the front of the tick
+                var aR = v * i + aT / 2;        // angle of the right side of the tick and protrusion
+                var aN = v * (i + 1) - aT / 2;  // angle of the left side of the NEXT tick, i.e. end of the between-wall
+
+                var framePieces = Ut.NewArray(
+                    // left face of the protrusion on the rim
+                    new[] { r(aL, rI, hT), r(aL, rO, hT), r(aL, rO, hP), r(aL, rI, hP) },
+                    // right face of the protrusion on the rim
+                    new[] { r(aR, rO, hT), r(aR, rI, hT), r(aR, rI, hP), r(aR, rO, hP) },
+                    // back face behind the protrusion
+                    new[] { r(aR, rO, 0), r(aR, rO, hP), r(aL, rO, hP), r(aL, rO, 0) },
+                    // top face of the protrusion
+                    new[] { r(aL, rI, hP), r(aL, rO, hP), r(aR, rO, hP), r(aR, rI, hP) },
+                    // front face of the protrusion
+                    new[] { r(aL, rI, hT), r(aL, rI, hP), r(aR, rI, hP), r(aR, rI, hT) },
+                    // top face of the tick
+                    new[] { r(aL, rI, hT), r(aR, rI, hT), r(aMid, rT, hT) },
+                    // left face of the tick
+                    new[] { r(aL, rI, 0), r(aL, rI, hT), r(aMid, rT, hT), r(aMid, rT, 0) },
+                    // right face of the tick
+                    new[] { r(aMid, rT, 0), r(aMid, rT, hT), r(aR, rI, hT), r(aR, rI, 0) },
+                    // front face of the between-wall
+                    new[] { r(aR, rI, 0), r(aR, rI, hW), r(aN, rI, hW), r(aN, rI, 0) },
+                    // top face of the between-wall
+                    new[] { r(aR, rI, hW), r(aR, rO, hW), r(aN, rO, hW), r(aN, rI, hW) },
+                    // back face of the between-wall
+                    new[] { r(aN, rO, 0), r(aN, rO, hW), r(aR, rO, hW), r(aR, rO, 0) }
+                )
+                    .Select(f => addTexture(f.Reverse().ToArray().FlatNormals()))
+                    .ToArray();
+
+                var dialPieces = Ut.NewArray(
+                    new[] { r(0, 0, 0), r(aL, rI, 0), r(aR, rI, 0) },
+                    new[] { r(0, 0, 0), r(aR, rI, 0), r(aN, rI, 0) }
+                )
+                    .Select(f => f.Reverse().ToArray().FlatNormals().Select(p => p.WithTexture((p.Location.X + 1) / 2 * 45 / 85, (p.Location.Z + 1) / 2)).ToArray())
+                    .ToArray();
+
+                return (framePieces, dialPieces);
+            });
+
+            return (data.SelectMany(x => x.framePieces), data.SelectMany(x => x.dialPieces));
         }
 
         public static void DoGraphics()
@@ -43,15 +119,15 @@ namespace KtaneStuff
             var names = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".Select(ch => ch.ToString()).Concat("R1,R2,R3,R4".Split(',')).ToArray();
             using (var bmp = new Bitmap(@"D:\c\KTANE\MaritimeFlags\Data\Flags.png"))
             {
-                var w = (bmp.Width - 20) / 8;
-                var h = (bmp.Height - 20) / 5;
+                var w = bmp.Width / 8;
+                var h = bmp.Height / 5;
                 for (int x = 0; x < 8; x++)
                     for (int y = 0; y < 5; y++)
                     {
                         GraphicsUtil.DrawBitmap(w, h, g =>
                         {
                             g.Clear(Color.Transparent);
-                            g.DrawImage(bmp, -x * w - 10, -y * h - 10);
+                            g.DrawImage(bmp, -x * w, -y * h);
                         }).Save($@"D:\c\KTANE\MaritimeFlags\Assets\Textures\Flag-{names[y * 8 + x]}-tmp.png");
                     }
             }
---
+++ Src/PatternCube.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using RT.Util.ExtensionMethods;
+
+namespace KtaneStuff
+{
+    static class PatternCube
+    {
+        private static readonly List<(int face1, int face2, int direction, int orientation)> _transitions;
+        static PatternCube()
+        {
+            _transitions = new List<(int face1, int face2, int direction, int orientation)>
+            {
+                (0, 1, 2, 0),
+                (0, 2, 1, 3),
+                (0, 3, 0, 2),
+                (0, 4, 3, 1),
+                (1, 2, 1, 0),
+                (1, 4, 3, 0),
+                (1, 5, 2, 2),
+                (2, 3, 1, 0),
+                (2, 5, 2, 1),
+                (3, 4, 1, 0),
+                (3, 5, 2, 0),
+                (4, 5, 2, 3)
+            };
+            foreach (var (face1, face2, direction, orientation) in _transitions.ToArray())
+                _transitions.Add((face2, face1, (direction + 6 - orientation) % 4, (4 - orientation) % 4));
+        }
+
+        private static int __debug_counter;
+        public static void Experiment()
+        {
+            __debug_counter = 0;
+            var results = new HashSet<string>();
+            for (int face0Orientation = 0; face0Orientation < 4; face0Orientation++)
+                recurse(results, new List<(int x, int y, int face, int orientation)> { (0, 0, 0, face0Orientation) });
+            File.WriteAllText(@"D:\temp\PatternCubeNets.txt", results.JoinString("\n\n"));
+            Console.WriteLine(__debug_counter);
+            Console.WriteLine(results.Count);
+
+            var allowableTopCombinations = ",X,Y,XY,XZ,XYZ".Split(',').SelectMany(str => "ABCD".Subsequences(3 - str.Length, 3 - str.Length).Select(ss => str + ss.JoinString())).ToArray();
+            var allowableBottomCombinations = ",X,Z,XY,YZ,XYZ".Split(',').SelectMany(str => "EFGH".Subsequences(3 - str.Length, 3 - str.Length).Select(ss => str + ss.JoinString())).ToArray();
+
+            Console.WriteLine($"{allowableTopCombinations.JoinString(" // ")} = {allowableTopCombinations.Length}");
+            Console.WriteLine($"{allowableBottomCombinations.JoinString(" // ")} = {allowableBottomCombinations.Length}");
+        }
+
+        private static readonly int[] _xDelta = new[] { 0, 1, 0, -1 };
+        private static readonly int[] _yDelta = new[] { -1, 0, 1, 0 };
+
+        private static void recurse(HashSet<string> results, List<(int x, int y, int face, int orientation)> sofar)
+        {
+            if (sofar.Count == 6)
+            {
+                var sb = new StringBuilder();
+                var minX = sofar.Min(k => k.x);
+                var maxX = sofar.Max(k => k.x);
+                var minY = sofar.Min(k => k.y);
+                var maxY = sofar.Max(k => k.y);
+
+                for (int sx = minX; sx <= maxX; sx++)
+                    for (int sy = minY; sy <= maxY; sy++)
+                        if (sofar.Any(t => t.x == sx && t.y == sy) && sofar.Any(t => t.x == sx + 1 && t.y == sy) && sofar.Any(t => t.x == sx && t.y == sy + 1) && sofar.Any(t => t.x == sx + 1 && t.y == sy + 1))
+                            return;
+
+                for (int sy = minY; sy <= maxY; sy++)
+                    sb.AppendLine(Enumerable.Range(minX, maxX - minX + 1).Select(sx =>
+                    {
+                        var ix = sofar.IndexOf(tuple => tuple.x == sx && tuple.y == sy);
+                        return ix == -1 ? "   " : $"{sofar[ix].face}{"NESW"[sofar[ix].orientation]} ";
+                    }).JoinString());
+                if (!results.Add(sb.ToString()))
+                    __debug_counter++;
+            }
+            else
+            {
+                foreach (var (face1, face2, direction, newOrientation) in _transitions)
+                {
+                    var ix = sofar.IndexOf(tp => tp.face == face1);
+                    if (ix == -1)
+                        continue;
+                    if (sofar.Any(tp => tp.face == face2))
+                        continue;
+                    var (x, y, face, oldOrientation) = sofar[ix];
+                    var newX = x + _xDelta[(direction + oldOrientation) % 4];
+                    var newY = y + _yDelta[(direction + oldOrientation) % 4];
+                    if (sofar.Any(tp => tp.x == newX && tp.y == newY))
+                        continue;
+                    var newSofar = sofar.ToList();
+                    newSofar.Add((newX, newY, face2, (newOrientation + oldOrientation) % 4));
+                    recurse(results, newSofar);
+                }
+            }
+        }
+    }
+}
