3114ce31c64a2299b6dd19b99d067960273c07e2
Timwi <github@timwi.de>
2018-07-23 17:02:06 +0200

    Pattern Cube developments

--- Src/PatternCube.cs
+++ Src/PatternCube.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using RT.Util;
@@ -11,24 +12,32 @@ namespace KtaneStuff
     {
         sealed class FaceSymbol : IEquatable<FaceSymbol>
         {
-            public char Symbol;
-            public int Orientation;
+            public char Symbol { get; private set; }
+            public int Orientation { get; private set; }
             public bool Equals(FaceSymbol other) => other != null && other.Symbol == Symbol && other.Orientation == Orientation;
+            public FaceSymbol(char symbol, int orientation) { Symbol = symbol; Orientation = orientation; }
         }
         sealed class HalfCube
         {
-            public FaceSymbol Top, Left, Front;
+            public FaceSymbol Top { get; private set; }
+            public FaceSymbol Left { get; private set; }
+            public FaceSymbol Front { get; private set; }
+            public HalfCube(FaceSymbol top, FaceSymbol left, FaceSymbol front) { Top = top; Left = left; Front = front; }
+
+            public static HalfCube Random(char[] combination, Random rnd) =>
+                new HalfCube(new FaceSymbol(combination[0], rnd.Next(0, 4)), new FaceSymbol(combination[1], rnd.Next(0, 4)), new FaceSymbol(combination[2], rnd.Next(0, 4)));
         }
         sealed class FaceInfo : IEquatable<FaceInfo>
         {
-            public int Face;
-            public int Orientation;
+            public int Face { get; private set; }
+            public int Orientation { get; private set; }
             public bool Equals(FaceInfo other) => other != null && other.Face == Face && other.Orientation == Orientation;
+            public FaceInfo(int face, int orientation) { Face = face; Orientation = orientation; }
         }
         sealed class Net : IEquatable<Net>
         {
-            public FaceInfo[,] Faces;
-
+            public FaceInfo[,] Faces { get; private set; }
+            public Net(FaceInfo[,] faces) { Faces = faces; }
             public bool Equals(Net other)
             {
                 if (other == null)
@@ -47,9 +56,9 @@ namespace KtaneStuff
                 return true;
             }
 
-            public string ToString((char symbol, int orientation)[] faceInfos, bool[] showSymbol, bool[] showOrientation)
+            public string ToString(FaceSymbol[] faceSymbols, bool[] showSymbol, bool[] showOrientation)
             {
-                if (faceInfos == null || faceInfos.Length != 6 || showSymbol == null || showSymbol.Length != 6 || showOrientation == null || showOrientation.Length != 6)
+                if (faceSymbols == null || faceSymbols.Length != 6 || showSymbol == null || showSymbol.Length != 6 || showOrientation == null || showOrientation.Length != 6)
                     throw new ArgumentException();
                 var sb = new StringBuilder();
                 var w = Faces.GetLength(0);
@@ -80,18 +89,21 @@ namespace KtaneStuff
                             sb.Append("    ││││││││││││"[i]);
                         // Face
                         else
-                            sb.Append(Faces[fx, fy] == null ? "    " : $" {(showSymbol[Faces[fx, fy].Face] ? faceInfos[Faces[fx, fy].Face].symbol : ' ')}{"NESW "[showOrientation[Faces[fx, fy].Face] ? (faceInfos[Faces[fx, fy].Face].orientation + Faces[fx, fy].Orientation) % 4 : 4]} ");
+                            sb.Append(Faces[fx, fy] == null ? "    " : $" {(showSymbol[Faces[fx, fy].Face] ? faceSymbols[Faces[fx, fy].Face].Symbol : ' ')}{"NESW "[showOrientation[Faces[fx, fy].Face] ? (faceSymbols[Faces[fx, fy].Face].Orientation + Faces[fx, fy].Orientation) % 4 : 4]} ");
                     }
                     sb.Append("\n");
                 }
                 return sb.ToString();
             }
-        }
 
-        private static readonly HalfCube[] _group1 = /*Diag-g1-start*/@"X2,C2,B1;B0,D1,C2;C2,A3,D1;Z1,D0,X0;X2,C0,A1;A2,C3,Y2;C1,D0,Y2;Y0,D0,X3;X0,Z2,C1;X1,C1,D0;X2,C3,Y0;D3,A0,B3;D1,B2,Y0;Y2,B0,A2;X1,A2,D2;C3,B3,A1;Z1,X3,B1;Y3,C1,B0;X1,A3,B1;Z1,A2,X0;Y0,X1,A1;X1,Y2,B0;A0,Y3,D0;D0,B0,X0"/*Diag-g1-end*/
-            .Split(';').Select(hci => hci.Split(',').Select(fc => new FaceSymbol { Symbol = fc[0], Orientation = fc[1] - '0' }).ToArray()).Select(arr => new HalfCube { Top = arr[0], Left = arr[1], Front = arr[2] }).ToArray();
-        private static readonly HalfCube[] _group2 = /*Diag-g2-start*/@"X0,E3,Y1;G2,E2,Z1;G1,X1,F0;F1,Z1,H2;Z3,F1,E2;H1,G1,F1;Z2,H3,G2;H1,E3,F0;G1,Z1,F1;Y3,Z3,E1;Z3,Y3,H1;G0,Z2,Y0;G1,E2,H3;E0,X2,G1;Y1,X3,G3;X3,H1,F0;Z0,H2,E3;E0,F0,G1;H3,X2,E2;G1,H3,X0;X1,E0,F0;X0,F0,Y2;X3,H3,Y0;F1,Y2,Z0"/*Diag-g2-end*/
-            .Split(';').Select(hci => hci.Split(',').Select(fc => new FaceSymbol { Symbol = fc[0], Orientation = fc[1] - '0' }).ToArray()).Select(arr => new HalfCube { Top = arr[0], Left = arr[1], Front = arr[2] }).ToArray();
+            public string Code
+            {
+                get
+                {
+                    return $"new Net(new FaceInfo[{Faces.GetLength(0)}, {Faces.GetLength(1)}] {{ {Enumerable.Range(0, Faces.GetLength(0)).Select(x => $@"{{ {Enumerable.Range(0, Faces.GetLength(1)).Select(y => Faces[x, y] == null ? "null" : $"new FaceInfo({Faces[x, y].Face}, {Faces[x, y].Orientation})").JoinString(", ")} }}").JoinString(", ")} }})";
+                }
+            }
+        }
 
         private static readonly List<(int face1, int face2, int direction, int orientation)> _transitions;
         static PatternCube()
@@ -122,69 +134,72 @@ namespace KtaneStuff
             for (int face0Orientation = 0; face0Orientation < 4; face0Orientation++)
                 recurse(nets, new List<(int x, int y, int face, int orientation)> { (0, 0, 0, face0Orientation) });
 
+            var netStrs = new HashSet<string>();
+            foreach (var nt in nets)
+                netStrs.Add(Enumerable.Range(0, nt.Faces.GetLength(1)).Select(y => Enumerable.Range(0, nt.Faces.GetLength(0)).Select(x => nt.Faces[x, y] == null ? " " : "#").JoinString()).JoinString("\n"));
+            File.WriteAllText(@"D:\Daten\Upload\KTANE\Pattern Cube nets.txt", netStrs.JoinString("\n\n"));
+            Console.WriteLine(netStrs.Count);
+
             // Generate diagrams in the manual
             var allowableGroup1Combinations = ",X,Y,XY,XZ,XYZ".Split(',').SelectMany(str => "ABCD".Subsequences(3 - str.Length, 3 - str.Length).Select(ss => str + ss.JoinString())).ToArray();
             var allowableGroup2Combinations = ",X,Z,XY,YZ,XYZ".Split(',').SelectMany(str => "EFGH".Subsequences(3 - str.Length, 3 - str.Length).Select(ss => str + ss.JoinString())).ToArray();
 
             var rnd = new Random(47);
-            List<((char symbol, int orientation) top, (char symbol, int orientation) left, (char symbol, int orientation) front)> generateCubes(char[][] combinations) =>
-                combinations.Select(combination => ((combination[0], rnd.Next(0, 4)), (combination[1], rnd.Next(0, 4)), (combination[2], rnd.Next(0, 4)))).ToList();
+            List<HalfCube> generateCubes(char[][] combinations) =>
+                combinations.Select(combination => HalfCube.Random(combination, rnd)).ToList();
 
             var group1Arrangements = generateCubes(allowableGroup1Combinations.Select(c => c.ToArray().Shuffle(rnd)).ToArray().Shuffle(rnd)).ToList().Shuffle(rnd);
             var group2Arrangements = generateCubes(allowableGroup2Combinations.Select(c => c.ToArray().Shuffle(rnd)).ToArray().Shuffle(rnd)).ToList().Shuffle(rnd);
 
             foreach (var (startTag, endTag, arrangements) in new[] { (@"<!-- g1-s -->", @"<!-- g1-e -->", group1Arrangements), (@"<!-- g2-s -->", @"<!-- g2-e -->", group2Arrangements) })
                 Utils.ReplaceInFile(@"D:\c\KTANE\Public\HTML\Pattern Cube.html", startTag, endTag,
-                    arrangements.Select(cube => $@"
-                        <div class='cube-box highlightable'>
-                            <div class='cube-rotation'>
-                                <div class='face front symbol-{cube.front.symbol} or-{cube.front.orientation}'></div>
-                                <div class='face left symbol-{cube.left.symbol} or-{cube.left.orientation}'></div>
-                                <div class='face top symbol-{cube.top.symbol} or-{cube.top.orientation}'></div>
-                            </div>
-                        </div>").Split(6).Select(chunk => $@"
-                    <div class='cube-row'>{chunk.JoinString()}
-                    </div>").JoinString("\n"));
+                    arrangements.Select(cube =>
+                        $"<div class='cube-box highlightable'><div class='cube-rotation'>" +
+                            $"<div class='face front symbol-{cube.Front.Symbol} or-{cube.Front.Orientation}'></div>" +
+                            $"<div class='face left symbol-{cube.Left.Symbol} or-{cube.Left.Orientation}'></div>" +
+                            $"<div class='face top symbol-{cube.Top.Symbol} or-{cube.Top.Orientation}'></div>" +
+                        $"</div></div>").Split(6).Select(chunk => $@"<div class='cube-row'>{chunk.JoinString()}</div>").JoinString("\n"));
 
             // Generate code file
-            //var path = @"D:\c\KTANE\KtaneStuff\Src\PatternCube.cs"; // TODO: change to module
-            //Utils.ReplaceInFile(path, "/*Diag-g1-" + "start*/", "/*Diag-g1-" + "end*/", $@"@""{group1Arrangements.Select(arr => new[] { arr.top, arr.left, arr.front }.Select(elem => elem.symbol + "" + elem.orientation).JoinString(",")).JoinString(";")}""");
-            //Utils.ReplaceInFile(path, "/*Diag-g2-" + "start*/", "/*Diag-g2-" + "end*/", $@"@""{group2Arrangements.Select(arr => new[] { arr.top, arr.left, arr.front }.Select(elem => elem.symbol + "" + elem.orientation).JoinString(",")).JoinString(";")}""");
+            var path = @"D:\c\KTANE\PatternCube\Assets\Data.cs";
+            Utils.ReplaceInFile(path, "/*Diag-g1-start*/", "/*Diag-g1-end*/", $@"@""{group1Arrangements.Select(halfCube => new[] { halfCube.Top, halfCube.Left, halfCube.Front }.Select(face => face.Symbol + "" + face.Orientation).JoinString(",")).JoinString(";")}""");
+            Utils.ReplaceInFile(path, "/*Diag-g2-start*/", "/*Diag-g2-end*/", $@"@""{group2Arrangements.Select(halfCube => new[] { halfCube.Top, halfCube.Left, halfCube.Front }.Select(face => face.Symbol + "" + face.Orientation).JoinString(",")).JoinString(";")}""");
+            Utils.ReplaceInFile(path, "// Nets-start", "// Nets-end", nets.Select(nt => nt.Code).JoinString(",\r\n") + "\r\n");
 
             // Generate a puzzle
-            var puzRnd = new Random(6);
+            var puzRnd = new Random(700000);
             var net = nets.PickRandom(puzRnd);
             var faceGivenByLine = Enumerable.Range(0, net.Faces.GetLength(1)).SelectMany(y => Enumerable.Range(0, net.Faces.GetLength(0)).Select(x => (x, y))).First(tup => net.Faces[tup.x, tup.y] != null).Apply(tup => net.Faces[tup.x, tup.y].Face);
             var (arrangement1, arrangement2) = puzRnd.Next(0, 2) == 0 ? (group1Arrangements, group2Arrangements) : (group2Arrangements, group1Arrangements);
-            var (top, left, front) = arrangement1.PickRandom(puzRnd);
-            var symbolsAlready = new[] { top.symbol, left.symbol, front.symbol };
-            var (fa, fb, fc) = arrangement2.Where(ag => !new[] { ag.top.symbol, ag.left.symbol, ag.front.symbol }.Intersect(symbolsAlready).Any()).PickRandom(puzRnd);
-            (char symbol, int orientation) right, back, bottom;
+            var halfCube1 = arrangement1.PickRandom(puzRnd);
+            var symbolsAlready = new[] { halfCube1.Top.Symbol, halfCube1.Left.Symbol, halfCube1.Front.Symbol };
+            var halfCube2 = arrangement2.Where(ag => !new[] { ag.Top.Symbol, ag.Left.Symbol, ag.Front.Symbol }.Intersect(symbolsAlready).Any()).PickRandom(puzRnd);
+            FaceSymbol right, back, bottom;
             switch (puzRnd.Next(0, 3))
             {
                 case 0:
-                    back = (fc.symbol, (fc.orientation + 3) % 4);
-                    right = (fa.symbol, (fa.orientation + 3) % 4);
-                    bottom = (fb.symbol, (fb.orientation + 3) % 4);
+                    back = new FaceSymbol(halfCube2.Front.Symbol, (halfCube2.Front.Orientation + 3) % 4);
+                    right = new FaceSymbol(halfCube2.Top.Symbol, (halfCube2.Top.Orientation + 3) % 4);
+                    bottom = new FaceSymbol(halfCube2.Left.Symbol, (halfCube2.Left.Orientation + 3) % 4);
                     break;
                 case 1:
-                    back = (fa.symbol, fa.orientation);
-                    right = (fb.symbol, (fb.orientation + 1) % 4);
-                    bottom = (fc.symbol, fc.orientation);
+                    back = new FaceSymbol(halfCube2.Top.Symbol, halfCube2.Top.Orientation);
+                    right = new FaceSymbol(halfCube2.Left.Symbol, (halfCube2.Left.Orientation + 1) % 4);
+                    bottom = new FaceSymbol(halfCube2.Front.Symbol, halfCube2.Front.Orientation);
                     break;
                 default:
-                    back = (fb.symbol, (fb.orientation + 2) % 4);
-                    right = (fc.symbol, (fc.orientation + 2) % 4);
-                    bottom = (fa.symbol, (fa.orientation + 1) % 4);
+                    back = new FaceSymbol(halfCube2.Left.Symbol, (halfCube2.Left.Orientation + 2) % 4);
+                    right = new FaceSymbol(halfCube2.Front.Symbol, (halfCube2.Front.Orientation + 2) % 4);
+                    bottom = new FaceSymbol(halfCube2.Top.Symbol, (halfCube2.Top.Orientation + 1) % 4);
                     break;
             }
             var faceGivenFully = (new[] { 0, 1, 4 }.Contains(faceGivenByLine) ? new[] { 2, 3, 5 } : new[] { 0, 1, 4 }).PickRandom(puzRnd);
-            var allFaces = new[] { top, front, right, back, left, bottom };
+            var allFaces = new[] { halfCube1.Top, halfCube1.Front, right, back, halfCube1.Left, bottom };
             Console.WriteLine(net.ToString(allFaces, Ut.NewArray(6, face => face == faceGivenByLine || face == faceGivenFully), Ut.NewArray(6, face => face == faceGivenFully)));
-            Console.WriteLine($"Symbols are: {allFaces.Select(f => f.symbol).ToArray().Shuffle(puzRnd).JoinString(", ")}");
+            Console.WriteLine($"Symbols are: {allFaces.Select(f => f.Symbol).ToArray().Shuffle(puzRnd).JoinString(", ")}");
             Console.WriteLine("Press Enter to view solution");
             Console.ReadLine();
-            Console.WriteLine(net.ToString(new[] { top, front, right, back, left, bottom }, Ut.NewArray(6, _ => true), Ut.NewArray(6, _ => true)));
+            Console.WriteLine(net.ToString(allFaces, Ut.NewArray(6, _ => true), Ut.NewArray(6, _ => true)));
         }
 
         private static readonly int[] _xDelta = new[] { 0, 1, 0, -1 };
@@ -210,9 +225,9 @@ namespace KtaneStuff
                     for (int sx = minX; sx <= maxX; sx++)
                     {
                         var ix = sofar.IndexOf(tuple => tuple.x == sx && tuple.y == sy);
-                        arr[sx - minX, sy - minY] = ix == -1 ? null : new FaceInfo { Face = sofar[ix].face, Orientation = sofar[ix].orientation };
+                        arr[sx - minX, sy - minY] = ix == -1 ? null : new FaceInfo(sofar[ix].face, sofar[ix].orientation);
                     }
-                nets.Add(new Net { Faces = arr });
+                nets.Add(new Net(arr));
             }
             else
             {
