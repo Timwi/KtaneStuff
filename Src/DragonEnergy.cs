9dda40f8e23bf5a6e346a9b984afa09113fcf80b
Timwi <github@timwi.de>
2018-08-22 22:32:26 +0200

    Forest and River characters have changed

--- Src/DragonEnergy.cs
+++ Src/DragonEnergy.cs
@@ -13,7 +13,7 @@ namespace KtaneStuff
     {
         public static void MakeSymbols()
         {
-            var data = @"Angry=怒;Dragon=龍;Friend=友;Loyal=忠;Heart=心;Blessing=福;Dream=夢;Hate=懟;Magic=魔;River=河;Child=子;Energy=能;Hope=願;Male=男;Emotion=感;Curse=咒;Female=女;Kind=類;Mountain=山;Soul=魂;Heaven=天;Force=力;Longevity=壽;Night=晚;Urgency=急;Delight=喜;Forest=林;Love=愛;Pure=純;Wind=風"
+            var data = @"Angry=怒;Dragon=龍;Friend=友;Loyal=忠;Heart=心;Blessing=福;Dream=夢;Hate=懟;Magic=魔;River=川;Child=子;Energy=能;Hope=願;Male=男;Emotion=感;Curse=咒;Female=女;Kind=類;Mountain=山;Soul=魂;Heaven=天;Force=力;Longevity=壽;Night=晚;Urgency=急;Delight=喜;Forest=森;Love=愛;Pure=純;Wind=風"
                 .Split(';').Select(str => str.Split('=')).OrderBy(arr => arr[0]).Select((arr, ix) => new { Name = arr[0], Character = arr[1] }).ToArray();
 
             foreach (var inf in data)
