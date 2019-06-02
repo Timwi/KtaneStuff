fb04031daa29382a8b712d220dc0d1b6e33f0c37
Timwi <github@timwi.de>
2019-06-02 19:34:57 +0200

    That big module image for TTS

---
+++ Src/ModulesImage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using RT.Util.ExtensionMethods;
+
+namespace KtaneStuff
+{
+    static class ModulesImage
+    {
+        public static void Update()
+        {
+            var modules = Ktane.GetLiveJson();
+            var ms = modules.Where(m => m["Type"].GetString() != "Widget" /*&& m["Name"].GetString() != "Encrypted Equations"*/).ToArray().Shuffle();
+            Utils.ReplaceInFile(@"D:\Daten\Upload\KTANE\Modules.html", "<!--%%-->", "<!--%%%-->", ms.Select(m => $"<div class='module' data-module='{m["Name"].GetString().HtmlEscape()}' data-type='{m["Type"].GetString().HtmlEscape()}'></div>").JoinString("\n"));
+            Console.WriteLine(ms.Length);
+        }
+    }
+}
